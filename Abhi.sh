657e053c78cd0531f04373a7044ad3bce7f3d9bd
Abhijit Patil <patilabhijit853@gmail.com>
2026-04-13 17:06:59 +0530

    this is my 2 commit

---
+++ Abhi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-33 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Git"
+echo "i am from pune"       
